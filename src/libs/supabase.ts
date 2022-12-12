cb66e6544fd9879ee2608c8caa24bfd0decd9fb4
kotatabe <qwerty789kota@gmail.com>
2022-12-12 13:17:39 +0900

    add: google login form

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export const getAllAnswerRanking = async () => {
   console.log(supabase.from('?'))
---
+++ src/pages/auth.tsx
@@ -0,0 +1,28 @@
+import { Auth } from '@supabase/auth-ui-react'
+import { Typography, Button } from '@supabase/ui'
+import { supabase } from '../libs/supabase'
+
+const Container = (props: any) => {
+  const { user } = Auth.useUser()
+  if (user)
+    return (
+      <>
+        <Typography.Text>Signed in: {user.email}</Typography.Text>
+        <Button block onClick={() => props.supabaseClient.auth.signOut()}>
+          Sign out
+        </Button>
+      </>
+    )
+  return props.children
+}
+
+export default function AuthBasic() {
+  return (
+    <div style={{ maxWidth: 400, margin: '0 auto', marginTop: 250 }}>
+
+      <Container supabaseClient={supabase}>
+        <Auth supabaseClient={supabase} providers={['google']} />
+      </Container>
+    </div>
+  )
+}
